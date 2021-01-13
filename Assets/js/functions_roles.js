75ffb4297adf5b46869d94d96675af450657044a
Eugenio Gosstl <eugeniogly@gmail.com>
2021-01-12 18:12:59 -0600

    DataTable
    Mostrar Roles de Usuario en DataTables con JSON

--- Assets/js/functions_roles.js
+++ Assets/js/functions_roles.js
@@ -1,3 +1,27 @@
+var tableRoles;
+document.addEventListener('DOMContentLoaded', function(){
+  tableRoles = $('#tableRoles').dataTable( {
+    "aProcessing":true,
+    "aServerSide":true,
+    "language": {
+        "url":  "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+    },
+    "ajax":{
+      "url": " "+base_url+"/Roles/getRoles",
+      "dataSrc":""
+    },
+    "columns":[
+      {"data":"idrol"},
+      {"data":"nombrerol"},
+      {"data":"descripcion"},
+      {"data":"estatus"}
+    ],
+    "responsive":"true",
+    "bDestroy": true,
+    "iDisplayLength": 10,
+    "order":[[0,"desc"]]
+  });
+});
 $('#tableRoles').DataTable();
 
 function openModal(){
